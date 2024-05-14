2f4ebb116995437b62281dc7476d339f83c88332
repo <bablu.anupam@gmail.com>
2024-05-14 10:30:32 +0530

    repo

--- resourse_group.tf
+++ resourse_group.tf
@@ -1,9 +1,9 @@
 # keyword   function                name of function -- this is not for providers 
-# resource "azurerm_resource_group" "anup-rsgrp" {
-#   name = "anup-resource-group1"
-#   location = "Central US"
-# }
-# using existing resources group 
-data "azurerm_resource_group" "anup-existing-rsgrp" {
-  name = "anup-manual-resourcesgrp"
+resource "azurerm_resource_group" "ashu-rsgrp" {
+  name = "ashu-resource-group1"
+  location = "Central US"
 }
+# using existing resources group 
+# data "azurerm_resource_group" "ashu-existing-rsgrp" {
+#   name = "ashu-manual-resourcesgrp"
+# }
